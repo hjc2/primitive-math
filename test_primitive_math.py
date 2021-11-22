03aa30a49bd76e4abf8c65643714bc4c5a67e7f3
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2021-11-22 02:17:49 -0600

    Update test_primitive_math.py

--- test_primitive_math.py
+++ test_primitive_math.py
@@ -1,26 +1,27 @@
 
-import primitive_math
 import pytest
+import primitive_math
 
-@pytest.mark.parametrize("test_x, expected", [(3, 4), (5,6), (12,13), (56, 57)])
+@pytest.mark.parametrize("test_x, expected", [(3, 4), (5, 6), (12, 13), (56, 57)])
 def test_increment(test_x, expected):
     assert(primitive_math.increment(test_x) == expected)
 
-@pytest.mark.parametrize("test_x, expected", [(4, 3), (6,5), (13,12), (57, 56)])
+
+@pytest.mark.parametrize("test_x, expected", [(4, 3), (6, 5), (13, 12), (57, 56)])
 def test_decrement(test_x, expected):
     assert(primitive_math.decrement(test_x) == expected)
-    
-@pytest.mark.parametrize("test_x, test_y ,expected", [(3, 4, 7), (2, 7, 9), (2,8,10), (56, 17, 73)])
 
+
+@pytest.mark.parametrize("test_x, test_y ,expected", [(3, 4, 7), (2, 7, 9), (2, 8, 10), (56, 17, 73)])
 def test_add(test_x, test_y, expected):
     assert(primitive_math.add(test_x, test_y) == expected)
-    
-@pytest.mark.parametrize("test_x, test_y ,expected", [(3, 4, 12), (2, 7, 14), (2,8,16), (56, 17, 952)])
 
+
+@pytest.mark.parametrize("test_x, test_y ,expected", [(3, 4, 12), (2, 7, 14), (2, 8, 16), (56, 17, 952)])
 def test_multiply(test_x, test_y, expected):
     assert(primitive_math.multiply(test_x, test_y) == expected)
-    
-@pytest.mark.parametrize("test_x, test_y ,expected", [(4, 3, 64), (2, 3, 8), (7,2,49), (5, 3, 125)])
 
+
+@pytest.mark.parametrize("test_x, test_y ,expected", [(4, 3, 64), (2, 3, 8), (7, 2, 49), (5, 3, 125)])
 def test_power(test_x, test_y, expected):
     assert(primitive_math.power(test_x, test_y) == expected)
