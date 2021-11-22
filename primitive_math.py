fb850cd358910bfe566701fd0123189cb8eada84
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2021-11-22 00:05:44 -0600

    multiplication
    added testing multiplication
    added multiplication function

--- primitive_math.py
+++ primitive_math.py
@@ -1,6 +1,13 @@
 
-def add(x, y):
+def add(x, y): #adds two numbers
     if(y > 0):
         return(1 + add(x, y - 1))
     else:
+        return(x)
+
+"""docstring"""
+def multiply(x, y):
+    if(y > 1):
+        return(add(x, multiply(x, y - 1)))
+    if(y == 1):
         return(x)
--- test_primitive_math.py
+++ test_primitive_math.py
@@ -7,3 +7,7 @@ import pytest
 def test_add(test_x, test_y, expected):
     assert(primitive_math.add(test_x, test_y) == expected)
     
+@pytest.mark.parametrize("test_x, test_y ,expected", [(3, 4, 12), (2, 7, 14), (2,8,16), (56, 17, 952)])
+
+def test_multiply(test_x, test_y, expected):
+    assert(primitive_math.multiply(test_x, test_y) == expected)
