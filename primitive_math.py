eefaf59206f7ae7f0b0ce059f328d9228c7ea9a7
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2021-11-22 01:59:29 -0600

    mkdocs

--- primitive_math.py
+++ primitive_math.py
@@ -1,6 +1,7 @@
 
 def increment(x):
-    """increments a number"""
+    """increments a number \n
+    f(x) = x + 1"""
     return(x + 1)
 
 def decrement(x):
@@ -9,15 +10,24 @@ def decrement(x):
     return(x - 1)
 
 def add(x, y):
-    """adds two numbers"""
+    """adds two numbers \n
+    f(x, y) = x + y
+    """
     if(y > 0):
         return(increment(add(x, y - 1)))
     else:
         return(x)
 
 def multiply(x, y):
-    """multiplies two numbers"""
+    """multiplies two numbers \n
+    f(x) = x * y
+    """
     if(y > 1):
         return(add(x, multiply(x, y - 1)))
     if(y == 1):
         return(x)
+    
+def power(x, y):
+    """takes the power of a number \n
+    f(x) = x^y
+    """
